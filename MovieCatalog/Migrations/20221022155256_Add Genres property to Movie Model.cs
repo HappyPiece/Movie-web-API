415212b8ea533106fd0f81e5169eb1fc6088cd62
Dmitrii Trofimov <dimas.08.trofim@gmail.com>
2022-10-22 22:57:43 +0700

    Add Genres property to Movie model
    Added property, created migration files

---
+++ MovieCatalog/Migrations/20221022155256_Add Genres property to Movie Model.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieCatalog.Migrations
+{
+    public partial class AddGenrespropertytoMovieModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "MovieId",
+                table: "genres",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_genres_MovieId",
+                table: "genres",
+                column: "MovieId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_genres_movies_MovieId",
+                table: "genres",
+                column: "MovieId",
+                principalTable: "movies",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_genres_movies_MovieId",
+                table: "genres");
+
+            migrationBuilder.DropIndex(
+                name: "IX_genres_MovieId",
+                table: "genres");
+
+            migrationBuilder.DropColumn(
+                name: "MovieId",
+                table: "genres");
+        }
+    }
+}
