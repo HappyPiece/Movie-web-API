eb4e4d15fc8c36437d1eddcbfa84616d69d49551
Dmitrii Trofimov <dimas.08.trofim@gmail.com>
2022-10-22 13:45:01 +0700

    Add authentication and authorization systems
    Installed JwtBearer NuGet package
    Configured Jwt in JwtConfigurations file
    Registered and enabled auth services in Program.cs

--- MovieCatalog/Program.cs
+++ MovieCatalog/Program.cs
@@ -1,6 +1,26 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MovieCatalog.Properties;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = JwtConfigurations.Issuer,
+        ValidateIssuer = true,
+        ValidAudience = JwtConfigurations.Audience,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        IssuerSigningKey = JwtConfigurations.GetSymmetricSecurityKey(),
+        ValidateIssuerSigningKey = true
+    };
+});
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,6 +38,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
