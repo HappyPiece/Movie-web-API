22a63995a119201c3acdb8c4842ab6d366eba091
Dmitrii Trofimov <dimas.08.trofim@gmail.com>
2022-10-23 17:48:31 +0700

    Implement register method in Auth controller
    Method throws NotImplementedException becuase there are no token creation mechenisms yet

---
+++ MovieCatalog/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieCatalog.DAL;
+using MovieCatalog.DAL.Models;
+
+namespace MovieCatalog.Controllers
+{
+    [Route("api/account/register")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly MovieCatalogDbContext _context;
+
+        public AuthController(MovieCatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> register(UserRegisterDTO userRegisterDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _context.Users.AddAsync(new User
+            {
+                Name = userRegisterDTO.name,
+                Email = userRegisterDTO.email,
+                Username = userRegisterDTO.userName,
+                Password = userRegisterDTO.password,
+                BirthDate = userRegisterDTO.birthDate,
+                Gender = (Gender)userRegisterDTO.gender
+            });
+            await _context.SaveChangesAsync();
+
+            return StatusCode(500, new NotImplementedException());
+        }
+    }
+}
