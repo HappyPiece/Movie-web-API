450a9c49d11b6982d29dc02d47f166bc6c38ce27
Dmitrii Trofimov <dimas.08.trofim@gmail.com>
2022-10-23 23:27:46 +0700

    Implement login method in Auth controller
    Register method now returns token too

--- MovieCatalog/Controllers/AuthController.cs
+++ MovieCatalog/Controllers/AuthController.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MovieCatalog.DAL;
 using MovieCatalog.DAL.Models;
+using MovieCatalog.DTO;
+using MovieCatalog.Properties;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace MovieCatalog.Controllers
 {
-    [Route("api/account/register")]
+    [Route("api/account")]
     [ApiController]
     public class AuthController : ControllerBase
     {
@@ -16,7 +21,7 @@ namespace MovieCatalog.Controllers
             _context = context;
         }
 
-        [HttpPost]
+        [HttpPost("register")]
         public async Task<IActionResult> register(UserRegisterDTO userRegisterDTO)
         {
             if (!ModelState.IsValid)
@@ -35,7 +40,51 @@ namespace MovieCatalog.Controllers
             });
             await _context.SaveChangesAsync();
 
-            return StatusCode(500, new NotImplementedException());
+            return login(new LoginCredentialsDTO
+            {
+                username = userRegisterDTO.userName,
+                password = userRegisterDTO.password
+            });
+        }
+
+        [HttpPost("login")]
+        public IActionResult login(LoginCredentialsDTO loginCredentialsDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return StatusCode(400, ModelState);
+            }
+            var identity = GetIdentity(loginCredentialsDTO.username, loginCredentialsDTO.password);
+            if (identity == null)
+            {
+                return StatusCode(400, "Invalid username or password");
+            }
+            var jwt = new JwtSecurityToken(
+                issuer: JwtConfigurations.Issuer,
+                audience: JwtConfigurations.Audience,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddMinutes(JwtConfigurations.Lifetime),
+                signingCredentials: new SigningCredentials(JwtConfigurations.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256)
+                );
+            return StatusCode(200, new { token = new JwtSecurityTokenHandler().WriteToken(jwt) });
+        }
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            var user = _context.Users.Where(x => x.Username == username && x.Password == password).FirstOrDefault();
+            if (user == null)
+            {
+                return null;
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Id.ToString()),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, Convert.ToBoolean(user.IsAdmin)?"Admin":"User")
+            };
+
+            return new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
         }
     }
+
 }
