0fb36ca10cb671f248470dc9228f95316523432a
Dmitrii Trofimov <dimas.08.trofim@gmail.com>
2022-10-24 11:47:33 +0700

    Implement method for getting user profile
    Implement getProfile endpoint in User controller to process getting profile requests

---
+++ MovieCatalog/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieCatalog.DAL;
+using MovieCatalog.DTO;
+
+namespace MovieCatalog.Controllers
+{
+    [Route("api/account/profile")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly MovieCatalogDbContext _context;
+        public UserController(MovieCatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult getProfile()
+        {
+            var user = _context.Users.Where(x => x.Id.ToString() == User.Identity.Name).SingleOrDefault();
+            var profile = new ProfileDTO
+            {
+                id = user.Id,
+                name = user.Name,
+                nickName = user.Username,
+                email = user.Email,
+                birthDate = user.BirthDate,
+                gender = user.Gender,
+                avatarLink = user.AvatarLink
+            };
+            return StatusCode(200, profile);
+        }
+    }
+}
