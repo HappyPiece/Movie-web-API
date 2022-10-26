5441335a25f2e3388828867e00ea57756ff609d2
Dmitrii Trofimov <dimas.08.trofim@gmail.com>
2022-10-26 23:48:04 +0700

    Correct typo in configuration string

--- MovieCatalog/Properties/JwtConfigurations.cs
+++ MovieCatalog/Properties/JwtConfigurations.cs
@@ -6,8 +6,8 @@ namespace MovieCatalog.Properties
     public class JwtConfigurations
     {
         public const string Issuer = "ApiJwtIssuer";
-        public const string Audience = "ApiJwtAufience";
-        public const string Key = "Kupil' muzhik shlyapu, a ona yemu kak raz";
+        public const string Audience = "ApiJwtAudience";
+        public const string Key = "Kupil muzhik shlyapu, a ona yemu kak raz";
         public const int Lifetime = 3000;
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
